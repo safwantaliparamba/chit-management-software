694c34a55a4c3324c9229374e5a3de23c73cbdb3
safwanp <safwan.ilmlearn@gmail.com>
2024-05-31 17:43:20 +0530

    ---

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,14 @@
+"use client";
+
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-    return <div>Home</div>;
+    return <Container>Home</Container>;
 };
 
 export default Home;
+
+const Container = styled.div`
+    padding: 20px;
+`;
